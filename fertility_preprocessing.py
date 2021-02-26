85a87cf77ade1ce3cee22f3f68fed94b656b6067
oganesyankarina <oganesyankarina@mail.ru>
2021-02-26 15:50:42 +0300

    корректировки оформления

--- fertility_preprocessing.py
+++ fertility_preprocessing.py
@@ -1,24 +1,21 @@
 import warnings
 import pandas as pd
-# import numpy as np
 
-from datetime import date, datetime
-# from dateutil import relativedelta as rdelta
-# from operator import is_not
-# from functools import partial
+from datetime import datetime
 from create_connect_bd import cnx
 
 warnings.filterwarnings('ignore')
 
 
-def fertility_preprocessing():
+def fertility_preprocessing(save_to_sql=False, save_to_excel=False):
     df = pd.read_sql_query('''select * from fertility''', cnx)
     df[['mother_year_of_birth', 'due_date_year',
         'due_date_month', 'mother_age']] = df[['mother_year_of_birth', 'due_date_year',
                                                'due_date_month', 'mother_age']].astype('int64')
-    print(df.mother_address.sample(5))
+    print(f'Количество записей: {len(df)}')
     df = df[~df.mother_address.isin([''])]
     df.index = range(df.shape[0])
+    print(f'Количество записей после удаления строк без адресов: {len(df)}')
 ########################################################################################################################
 # Первый прогон по адресам
     print('Первая обработка mother_address...')
@@ -30,22 +27,14 @@ def fertility_preprocessing():
         for region in REGION:
             ind_str = df.loc[i, 'mother_address'].lower().find(region)
             df.loc[i, region] = ind_str
-    print(f'Elapsed time {datetime.now() - start_time}')
-    print(df[['mother_address', 'липецк ', 'елец ', 'воловский', 'грязинский', 'данковский', 'добринский', 'добровский',
-              'долгоруковский', 'елецкий', 'задонский', 'измалковский', 'краснинский', 'лебедянский', 'лев-толстовский',
-              'липецкий', 'становлянский', 'тербунский', 'усманский', 'хлевенский', 'чаплыгинский']][:5])
-    print(len(df[['mother_address', 'липецк ', 'елец ', 'воловский', 'грязинский', 'данковский', 'добринский',
-                  'добровский', 'долгоруковский', 'елецкий', 'задонский', 'измалковский', 'краснинский', 'лебедянский',
-                  'лев-толстовский', 'липецкий', 'становлянский', 'тербунский', 'усманский', 'хлевенский',
-                  'чаплыгинский']]))
-    start_time = datetime.now()
     for i in df.index[:]:
         for col in REGION:
             if df.loc[i, col] >= 0:
                 df.loc[i, 'Region'] = col
                 break
     print(f'Elapsed time {datetime.now() - start_time}')
-    print(df[['mother_address', 'Region']].sample(5))
+    print(f'Количество записей: {len(df)}')
+    print(df[['mother_address', 'Region']].sample(3))
 ########################################################################################################################
 # Второй прогон по адресам
     print(f"Количество нераспознанных адресов: {len(df[df.Region.isna()][['mother_address', 'Region']][:])}")
@@ -56,52 +45,47 @@ def fertility_preprocessing():
                    'лебедянь': 'лебедянский', 'лев-толстой': 'лев-толстовский', 'становое': 'становлянский',
                    'тербуны': 'тербунский', 'усмань': 'усманский', 'хлевное': 'хлевенский', 'чаплыгин': 'чаплыгинский',
                    'лев толстой': 'лев-толстовский'}
-
     start_time = datetime.now()
     for i in df[df.Region.isna()].index[:]:
         for town in CITIES_dict.keys():
             ind_str = df.loc[i, 'mother_address'].lower().find(town)
             df.loc[i, CITIES_dict[town]] = ind_str
-    print(f'Elapsed time {datetime.now() - start_time}')
-    print(df[df.Region.isna()][['mother_address', 'Region',
-                                'липецк ', 'елец ', 'воловский', 'грязинский', 'данковский', 'добринский', 'добровский',
-                                'долгоруковский', 'елецкий', 'задонский', 'измалковский', 'краснинский', 'лебедянский',
-                                'лев-толстовский', 'липецкий', 'становлянский', 'тербунский', 'усманский', 'хлевенский',
-                                'чаплыгинский']][:5])
-    print(len(df[df.Region.isna()][['mother_address', 'Region',
-                                    'липецк ', 'елец ', 'воловский', 'грязинский', 'данковский', 'добринский',
-                                    'добровский', 'долгоруковский', 'елецкий', 'задонский', 'измалковский',
-                                    'краснинский', 'лебедянский', 'лев-толстовский', 'липецкий', 'становлянский',
-                                    'тербунский', 'усманский', 'хлевенский', 'чаплыгинский']]))
-
-    start_time = datetime.now()
     for i in df[df.Region.isna()].index[:]:
         for col in REGION:
             if df.loc[i, col] >= 0:
                 df.loc[i, 'Region'] = col
                 break
     print(f'Elapsed time {datetime.now() - start_time}')
-
-    print(df[['mother_address', 'Region']].sample(5))
+    print(f'Количество записей: {len(df)}')
+    print(df[['mother_address', 'Region']].sample(3))
 ########################################################################################################################
 # Удаляем строки с адресами за пределами Липецкой области
-    print(f'Количество нераспознанных адресов: {len(df[df.Region.isna()])}')
+    print(f"Количество нераспознанных адресов: {len(df[df.Region.isna()][['mother_address', 'Region']][:])}")
     df = df[~df.Region.isna()]
     df.index = range(df.shape[0])
 ########################################################################################################################
 # Финальная обработка названий районов
     print('Финальная обработка названий районов...')
-    print(len(df.Region.unique()))
+    print(f'Количество районов: {len(df.Region.unique())}')
     print(sorted(df.Region.unique()))
     df.Region = df.Region.str.title()
     print(sorted(df.Region.unique()))
-
+    df = df.drop(columns=REGION)
+    print(f'Количество записей: {len(df)}')
+########################################################################################################################
+# Сохраняем предобработанные данные
+    if save_to_sql:
+        print('Сохраняем данные в базу данных...')
+        # df_death.to_sql('death_finished', cnx, if_exists='replace', index_label='id')
+    if save_to_excel:
+        print('Сохраняем данные в файл...')
+        path = r'C:\Users\oganesyanKZ\PycharmProjects\fertility\Рассчеты/'
+        with pd.ExcelWriter(f'{path}fertility_preprocessed.xlsx', engine='openpyxl') as writer:
+            df.to_excel(writer, sheet_name=f'fertility_preprocessed', header=True, index=False, encoding='1251')
+########################################################################################################################
     return df
-
-# table = pd.pivot_table(df, index=['due_date_year'], columns=['sex'], values=['which_born'],
-# aggfunc='sum', margins=True)
-# table
+########################################################################################################################
 
 
 if __name__ == '__main__':
-    fertility_preprocessing()
+    fertility_preprocessing(save_to_excel=True)
